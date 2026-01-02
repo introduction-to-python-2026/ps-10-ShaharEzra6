27d6aa5e1e83334f57d953e2392e747932af1078
Shahar Ezra <ezra.shahar.2004@gmail.com>
2026-01-02 21:11:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from image_utils2 import load_image, edge_detection
+from skimage.filters import median
+from skimage.morphology import disk
 
+shahar_img = load_image("baby_me.JPG")
+shahar_edge = edge_detection(shahar_img)
+shahar_clean = median(shahar_edge, disk(3))
+threshold = 100  # adjust if edges look too few or too many
+edge_binary = shahar_clean > threshold
+edge_image = Image.fromarray(edge_binary)
+edge_image.save('my_edges.png')
